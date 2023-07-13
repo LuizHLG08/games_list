8798c952440cef19af8e8fa22936c3a4db9390cb
LuizHLG <luizlelek08@gmail.com>
2023-07-13 16:53:04 -0300

    GET lists by id

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repositories.GameRepository;
 
 @Service
@@ -24,10 +25,16 @@ public class GameService {
         return dto;
     }
 
-    
+    @Transactional(readOnly = true)
     public List <GameMinDTO> findAll() {
         List <Game> result = gameRepository.findAll();
         List <GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
         return dto;
+    }
+
+    @Transactional(readOnly = true)
+    public List <GameMinDTO> findByList(Long listId) {
+        List <GameMinProjection> result = gameRepository.searchByList(listId);
+        return result.stream().map(x -> new GameMinDTO(x)).toList();
     }
 }
